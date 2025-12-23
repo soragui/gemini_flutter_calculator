0ea954f4668d903927d2fea5be047b3e21d0ca84
soragui <soragui@nativeng.org>
2025-12-23 21:41:47 +0800

    feat: Implement a basic calculator application with Flutter and refactor code.

---
+++ lib/logic/calculator_logic.dart
@@ -0,0 +1,39 @@
+import 'package:math_expressions/math_expressions.dart';
+
+class CalculatorLogic {
+  String _expression = '';
+  String _result = '';
+
+  String get expression => _expression;
+  String get result => _result;
+
+  void buttonPressed(String buttonText) {
+    if (buttonText == 'C') {
+      _expression = '';
+      _result = '';
+    } else if (buttonText == '=') {
+      try {
+        Parser p = Parser();
+        Expression exp = p.parse(_expression
+            .replaceAll('×', '*')
+            .replaceAll('÷', '/')
+            .replaceAll('π', '3.1415926535897932')
+            .replaceAll('√', 'sqrt')
+            .replaceAll('x²', '^2')
+            .replaceAll('mod', '%'));
+        ContextModel cm = ContextModel();
+        _result = exp.evaluate(EvaluationType.REAL, cm).toString();
+      } catch (e) {
+        _result = 'Error';
+      }
+    } else {
+      _expression += buttonText;
+    }
+  }
+
+  void undo() {
+    if (_expression.isNotEmpty) {
+      _expression = _expression.substring(0, _expression.length - 1);
+    }
+  }
+}
---
+++ lib/ui/widgets/calculator_app_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+class CalculatorAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final VoidCallback onUndo;
+
+  const CalculatorAppBar({super.key, required this.onUndo});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: DragToMoveArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.undo),
+              onPressed: onUndo,
+            ),
+            const Text('Basic'),
+            const Icon(Icons.arrow_drop_down),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.grey[200],
+      elevation: 0,
+      foregroundColor: Colors.black,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
